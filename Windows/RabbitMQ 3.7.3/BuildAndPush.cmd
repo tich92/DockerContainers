589a4f98710c9adfce75c3acd782a35be4cf2e45
Arsen <timchuk201092@gmail.com>
2018-02-19 13:42:50 +0200

    Add RabbitMQ v3.7.3 docker image for windows

---
+++ Windows/RabbitMQ 3.7.3/BuildAndPush.cmd
@@ -0,0 +1,4 @@
+REM tich92/rabbitmq-windowsservercore
+docker build . -t tich92/rabbitmq-windowsservercore
+docker push tich92/rabbitmq-windowsservercore
+docker rmi tich92/rabbitmq-windowsservercore
